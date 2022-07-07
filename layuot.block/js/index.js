6ebfc09502b16083b25b87f753212398155ca3d7
vladislav <vlad.safonenko@mail.ru>
2022-07-07 22:03:36 +0300

    creating alert and function

--- layuot.block/js/index.js
+++ layuot.block/js/index.js
@@ -1,14 +1,23 @@
-const usName=prompt('Ваше имя:');
-const surname=prompt('Ваше фамилия:');
+const usName = prompt("Ваше имя:");
+const surname = prompt("Ваше фамилия:");
 const patronymice = prompt("Ваше отчество:");
-const ageYear = prompt('Ваше возрост в годах:');
-const pol= confirm('Ваш пол:мужской?');
+const ageYear =
+  prompt("Ваше возрост в годах:") >= 18
+    ? alert("you are big boy")
+    : alert("you are baby");
+const pol = confirm("Ваш пол:мужской?");
 const pension = confirm("Вы на пенсии?");
 
-  if (ageYear < 18) {
-  
-alert('you are baby');
-}
-else{
-  alert('you are big boy')
+alert(`Ваше имя:${usName},
+Ваше фамилия:${surname} ,
+Ваше отчество:${patronymice},
+Ваше возрост в годах:${ageYear},
+Ваш возрост в днях: ${ageYear * 365},
+Ваш пол:мужской?${pol},
+Вы на пенсии?${pension}`);
+
+if (ageYear < 18) {
+  alert("you are baby");
+} else {
+  alert("you are big boy");
 }
